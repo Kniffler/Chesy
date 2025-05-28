0f4cb8ff8ce9208fc6c445b4e780d07dee1cdaff
Kniffler <character.lumenit@gmail.com>
2025-05-28 18:15:10 +0100

    modified:   q.lua

--- modules/q.lua
+++ modules/q.lua
@@ -40,7 +40,30 @@ function fs.execute(message)
 		
 		saveChannels(channelLinks)
 		actualReply(message, "Unregistered link. New messages will NEVER appear here")
+
+	elseif specify == "reregister" then
+		if not #args == 3 then log("Inaccurate argument count for reregister : Abandoning")
+		actualyReply(message, "Please provide the proper amount of arguments") return end
+
+		if not channelLinks[guild][tostring(channelID).."c"] then
+			log("Channel has no entry or is nil somehow")
+			actualyReply(message, "You have no link to disengage, try running this command again in the control channel")
+			return
+		end
+		channelLinks[guild][(channelLinks[guild][channelID.."c"]).."o"] = nil
+		channelLinks[guild][channelID.."c"] = nil
 		
+		local origin = args[3]
+		
+		local control = message.channel.id
+		if not channelLinks[guild] then channelLinks[guild] = {} end
+
+		channelLinks[guild][origin.."o"] = control
+		channelLinks[guild][control.."c"] = origin
+		
+		saveChannels(channelLinks)
+		actualReply(message, "Reregistered link.")
+					
 	elseif specify == "unregister-guild" then
 		if not #args == 2 then log("Inaccurate argument count for unregister-guild : Abandoning")
 		actualyReply(message, "Please provide the proper amount of arguments") return end
